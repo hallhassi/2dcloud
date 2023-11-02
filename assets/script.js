e9c625fac8a99bf5e21504d41601814826c1a2c6
hallhassi <hallhassi@gmail.com>
2023-11-02 03:40:31 +0000

    a

--- assets/script.js
+++ assets/script.js
@@ -112,7 +112,7 @@ function checkboxFunction() {
     if (checkbox.checked) {
         window.addEventListener('scroll', draw, { passive: true })
         stylesheet.href = checkbox.dataset.scroll
-        spacer.style.display = 'initial'
+        spacer.style.display = 'inherit'
     } else {
         window.removeEventListener('scroll', draw, { passive: true })
         stylesheet.href = checkbox.dataset.list
