b6d1f0e754d8b14791e93478588b92f4773ebbdd
hallhassi <hallhassi@gmail.com>
2023-11-02 19:20:54 +0000

    a

--- assets/script.js
+++ assets/script.js
@@ -129,13 +129,12 @@ if (history.state) storedHandle = history.state.handle
 
 Array.from(summaries).forEach(summary => {
     summary.addEventListener('click', (e) => {
-        console.log(e.currentTarget, e.currentTarget.parentNode.open, e.target);
-        if (e.currentTarget.parentNode.dataset?.handle == storedHandle) closeCart()
-        else if (e.currentTarget.parentNode.dataset?.handle) {
-            closeItems()
-            pushState(e.currentTarget.parentNode.dataset.handle)
-        } 
-        else if (e.target.tagName == 'BODY') closeItems()
+        if (e.currentTarget.parentNode.dataset?.handle)
+            if (e.currentTarget.parentNode.dataset?.handle == storedHandle) closeCart()
+            else {
+                closeItems()
+                pushState(e.currentTarget.parentNode.dataset.handle)
+            } else if (e.target.tagName == 'BODY') closeItems()
         else if (e.currentTarget.parentNode.id == 'cart' && document.body.querySelector('.item[open]')) closeItems()
     })
 })
