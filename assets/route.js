290de2ebe500060236eaa0e014823d334943c324
hallhassi <hallhassi@gmail.com>
2023-11-04 18:43:07 +0000

    s

--- assets/route.js
+++ assets/route.js
@@ -1,8 +1,17 @@
-var js = document.createElement("script");
-js.type = "text/javascript";
+var css = document.createElement("link")
+css.rel = "stylesheet"
+css.type = "text/css"
+css.media = "all"
+var js = document.createElement("script")
+js.type = "text/javascript"
 
 if (typeof productId == 'undefined') {
-    js.src = indexScript;
-} else  js.src = "";
+    js.src = indexScript
+    css.src = indexCss
+} else {
+    css.src = listCss
+    js.src = ""
+}
 
-document.body.appendChild(js);
+document.head.appendChild(css)
+document.body.appendChild(js)
