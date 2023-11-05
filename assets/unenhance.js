086a97802407e75079c2761a8b3055c4661112f6
hallhassi <hallhassi@gmail.com>
2023-11-05 03:28:42 +0000

    a

--- assets/unenhance.js
+++ assets/unenhance.js
@@ -1,4 +1,4 @@
 enhanced = false
 window.removeEventListener('scroll', handleScroll, { passive: true });
 window.removeEventListener('resize', handleResize);
-Array.from(items).forEach(x => x.width = "")
+Array.from(items).forEach(x => x.style.width = "")
